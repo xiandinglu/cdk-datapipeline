7c053d27e8b73a4554bbda0553a74a6130ca7a00
xiandinglu <xianding6lu@gmail.com>
2023-03-12 22:24:53 +0800

    test pre commit

--- cdk_datapipeline/cdk_glue_stack.py
+++ cdk_datapipeline/cdk_glue_stack.py
@@ -1,23 +1,27 @@
-from aws_cdk import (
-    Stack,
-    aws_glue as glue,
-)
+"""This Stack is to create the ETL pipeline by using glue services"""
+
+from aws_cdk import Stack
+from aws_cdk import aws_glue as glue
 from constructs import Construct
 
+
 class CdkGlueStack(Stack):
-    
+    """Create Glue Jobs and Glue Trigger"""
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
+
         # Create a Glue Job
-        job1 = glue.CfnJob(
-            self, "GlueJob1",
+        glue.CfnJob(
+            self,
+            "GlueJob1",
             command=glue.CfnJob.JobCommandProperty(
                 name="glueetl",
-                script_location="s3://testing-script-1234/glue-script/testing-glue-script"
+                script_location="""
+                    s3://testing-script-1234/glue-script/testing-glue-script
+                """,
             ),
             role="your_glue_role_arn",
-
             # the properties below are optional
             # allocated_capacity=123,
             # connections=glue.CfnJob.ConnectionsListProperty(
@@ -28,7 +32,9 @@ class CdkGlueStack(Stack):
                 "--job-name": "Job1",
                 "--TempDir": "s3://testing-script-1234/glue-temp/temp1",
                 "--job-bookmark-option": "job-bookmark-disable",
-                "--extra-files": "s3://testing-script-1234/log4j-properties-config/log4j.properties"
+                "--extra-files": """
+                    s3://testing-script-1234/log4j-properties-config/log4j.properties
+                """,
             },
             # description="description",
             # execution_class="executionClass",
@@ -44,18 +50,20 @@ class CdkGlueStack(Stack):
             # ),
             number_of_workers=2,
             timeout=2,
-            worker_type="G.1X"
+            worker_type="G.1X",
         )
 
         # Create second Glue Job
-        job2 = glue.CfnJob(
-            self, "GlueJob2",
+        glue.CfnJob(
+            self,
+            "GlueJob2",
             command=glue.CfnJob.JobCommandProperty(
                 name="glueetl",
-                script_location="s3://testing-script-1234/glue-script/testing-glue-script"
+                script_location="""
+                    s3://testing-script-1234/glue-script/testing-glue-script
+                """,
             ),
             role="your_glue_role_arn",
-
             # the properties below are optional
             # allocated_capacity=123,
             # connections=glue.CfnJob.ConnectionsListProperty(
@@ -66,7 +74,9 @@ class CdkGlueStack(Stack):
                 "--job-name": "Job1",
                 "--TempDir": "s3://testing-script-1234/glue-temp/temp2",
                 "--job-bookmark-option": "job-bookmark-disable",
-                "--extra-files": "s3://testing-script-1234/log4j-properties-config/log4j.properties"
+                "--extra-files": """
+                    s3://testing-script-1234/log4j-properties-config/log4j.properties
+                """,
             },
             # description="description",
             # execution_class="executionClass",
@@ -82,19 +92,22 @@ class CdkGlueStack(Stack):
             # ),
             number_of_workers=2,
             timeout=2,
-            worker_type="G.1X"
+            worker_type="G.1X",
         )
 
         # Create a Glue Trigger
-        trigger1 = glue.CfnTrigger(
-            self, "GlueTrigger1",
-            actions=[glue.CfnTrigger.ActionProperty(
-                job_name="GlueJob1",
-                notification_property=glue.CfnTrigger.NotificationPropertyProperty(
-                    notify_delay_after=2
-                ),
-                timeout=2
-            )],
+        glue.CfnTrigger(
+            self,
+            "GlueTrigger1",
+            actions=[
+                glue.CfnTrigger.ActionProperty(
+                    job_name="GlueJob1",
+                    notification_property=glue.CfnTrigger.NotificationPropertyProperty(
+                        notify_delay_after=2
+                    ),
+                    timeout=2,
+                )
+            ],
             type="SCHEDULED",
             name="GlueTrigger1",
             schedule="cron(0 0 * * ? *)",
@@ -102,23 +115,28 @@ class CdkGlueStack(Stack):
         )
 
         # Create second Glue Trigger
-        trigger2 = glue.CfnTrigger(
-            self, "GlueTrigger2",
-            actions=[glue.CfnTrigger.ActionProperty(
-                job_name="GlueJob2",
-                notification_property=glue.CfnTrigger.NotificationPropertyProperty(
-                    notify_delay_after=2
-                ),
-                timeout=2
-            )],
+        glue.CfnTrigger(
+            self,
+            "GlueTrigger2",
+            actions=[
+                glue.CfnTrigger.ActionProperty(
+                    job_name="GlueJob2",
+                    notification_property=glue.CfnTrigger.NotificationPropertyProperty(
+                        notify_delay_after=2
+                    ),
+                    timeout=2,
+                )
+            ],
             type="CONDITIONAL",
             name="GlueTrigger2",
             predicate=glue.CfnTrigger.PredicateProperty(
-                conditions=[glue.CfnTrigger.ConditionProperty(
-                    job_name="GlueJob1",
-                    logical_operator="EQUALS",
-                    state="SUCCEEDED"
-                )],
+                conditions=[
+                    glue.CfnTrigger.ConditionProperty(
+                        job_name="GlueJob1",
+                        logical_operator="EQUALS",
+                        state="SUCCEEDED",
+                    )
+                ],
             ),
             start_on_creation=False,
         )
